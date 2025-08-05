95936314c6ac40ac20ebb5320837eead936c1ee3
Employee pranay <reddypranay71@gmail.com>
2025-08-05 09:48:20 +0530

    commited

---
+++ src/Employee.java
@@ -0,0 +1,39 @@
+public class Employee {
+
+    private String name;
+    private Integer id;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
