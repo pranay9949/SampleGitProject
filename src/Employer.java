38afdc74c3aa32d8047f1106280e51e98510a4b4
Employee pranay <reddypranay71@gmail.com>
2025-08-05 09:59:19 +0530

    Added some changes and added files

---
+++ src/Employer.java
@@ -0,0 +1,20 @@
+public class Employer {
+    private String companyName;
+    private Integer companyId;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+}
